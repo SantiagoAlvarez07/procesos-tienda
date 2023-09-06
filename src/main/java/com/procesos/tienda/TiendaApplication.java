49f70e6d916578d4ffe9dce0ec9271f6c3801626
Santiago Alvarez <colombia@hotmail.com>
2023-09-05 21:10:18 -0500

    creacion BD_v3

--- src/main/java/com/procesos/tienda/TiendaApplication.java
+++ src/main/java/com/procesos/tienda/TiendaApplication.java
@@ -2,6 +2,7 @@ package com.procesos.tienda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 public class TiendaApplication {
